36cd8f5773c4520e851327341662c377d8b3778c
ShahriarArefin <arfin17310@gmail.com>
2025-02-18 23:23:43 +0600

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
    return (
       <main className="py-6 container min-h-full grid grid-cols-[1fr_24rem] gap-6 content-start grid-rows-[auto_1fr]">
          <header className="relative p-6 h-80 bg-background shadow-card rounded-xl col-start-1 col-span-1 overflow-hidden">
-            <h1>Hello World</h1>
+            <Image
+               fill
+               priority
+               src={`/Cover.jpg`}
+               alt="Cover Photo"
+               className="w-full object-cover"
+            />
          </header>
 
          <aside className="p-6 bg-background shadow-card rounded-xl col-start-2 col-span-1 row-span-full">
